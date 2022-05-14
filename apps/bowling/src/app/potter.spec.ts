d7a623c67f1cf670c19b4470fe988c6422035409
nomiaro <derekt.cs06@nctu.edu.tw>
2022-05-14 23:08:19 +0800

    potter 1st red

--- apps/bowling/src/app/potter.spec.ts
+++ apps/bowling/src/app/potter.spec.ts
@@ -4,4 +4,11 @@ describe('Potter', () => {
   it('should create an instance', () => {
     expect(new Potter()).toBeTruthy();
   });
+
+  test('buy one book', () => {
+    const potter = new Potter();
+    potter.buy(1);
+
+    expect(potter.price).toBe(100);
+  });
 });
--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,2 +1,9 @@
 export class Potter {
+  buy(book: number) {
+    throw new Error('Method not implemented.');
+  }
+
+  get price() {
+      return 0;
+  }
 }
