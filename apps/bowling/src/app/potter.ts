ef07fea9dfb3621e31c0f568aa8e6cca0de62020
nomiaro <derekt.cs06@nctu.edu.tw>
2022-05-15 00:04:00 +0800

    potter 3rd green

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -8,9 +8,15 @@ export class Potter {
     get price() {
         let price = 0;
         let num_diff_books = -1;
+        let unique_books: number[] = [];
+        let currentindex = 0;
+
         for (let bookindex = 0; bookindex < this.books.length; bookindex++){
             price += 100;
-            num_diff_books++;
+            if (!(unique_books.includes(this.books[bookindex]))){
+                num_diff_books++;
+                unique_books[currentindex++] = this.books[bookindex]
+            }
         }
 
         let cost = 1 - 0.05*num_diff_books;
